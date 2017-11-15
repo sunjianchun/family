2e638b5c21a473a5a34dc6d3f7cf7f65d3aebc5c
sunjianchun <sunjianchun@sunjianchundeMacBook-Pro-2.local>
2017-11-15 10:31:32 +0800

    page

--- family.go
+++ family.go
@@ -1,9 +1,8 @@
 package main
 
 import (
+	inner_http "family/http"
 	"family/middleware"
-	"family/oauth2"
-	"family/person"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -17,22 +16,22 @@ func main() {
 
 	//person相关路由
 	p := router.Group("/api/person")
-	p.GET("/get", person.Get)
-	p.GET("/list", person.List)
-	p.GET("/import", person.Import)
+	p.GET("/get", inner_http.Get)
+	p.GET("/list", inner_http.List)
+	p.GET("/import", inner_http.Import)
 
-	p.GET("/flush", person.Flush)
-	p.GET("/getChildren", person.GetChildren)
-	p.GET("/getPosterity", person.GetAllPosterity)
+	p.GET("/flush", inner_http.Flush)
+	p.GET("/getChildren", inner_http.GetChildren)
+	p.GET("/getPosterity", inner_http.GetAllPosterity)
 
 	//page info相关路由
 	info := router.Group("/info")
+
 	info.StaticFile("/list", "./templates/info/list.html")
-	info.LoafHTMLGlob("templates/*")
 
 	//oauth2相关路由
 	o := router.Group("/oauth2")
-	o.GET("/showcode", oauth2.ShowCode)
+	o.GET("/showcode", inner_http.ShowCode)
 	router.GET("/index", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "welcome to sunshijiapu.com.cn"})
 	})
--- http/person.go
+++ http/person.go
@@ -1,4 +1,4 @@
-package person
+package http
 
 import (
 	"family/conf"
